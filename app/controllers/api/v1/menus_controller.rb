1ffa52003e71433442049d2c73b4d58735800d88
yokonao <yokotukanao@me.com>
2021-08-02 21:41:57 +0900

    enable seaching menus by date

--- app/controllers/api/v1/menus_controller.rb
+++ app/controllers/api/v1/menus_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::MenusController < ApplicationController
   def index
     @menus = Menu.all.order(start_at: :asc)
+    @menus = @menus.where(start_at: Time.parse(date_param)..(Time.parse(date_param) + 1.days)) if date_param
     render json: @menus
+  end
+
+  def date_param
+    params.permit(:date)[:date]
   end
 end
--- spec/requests/api/v1/menus_spec.rb
+++ spec/requests/api/v1/menus_spec.rb
@@ -4,18 +4,29 @@ RSpec.describe 'Api::V1::Menus', type: :request do
   let(:base_day) { Date.parse('2021-09-05') }
   describe 'GET /index' do
     before do
-      (1...10).each do |i|
+      (1...7).each do |i|
         CreateDailyAppointmentMenuService.new(base_day + i.days).execute
       end
     end
     subject do
-      get api_v1_menus_index_path, as: :json
+      get api_v1_menus_index_path, as: :json, params: params
       response
     end
     let(:json) { JSON.parse(response.body) }
-    it 'returns all menu' do
-      expect(subject).to have_http_status(:ok)
-      expect(json.length).to eq 88
+    context 'when no prameter is given' do
+      let(:params) { nil }
+      it 'returns all menu' do
+        expect(subject).to have_http_status(:ok)
+        expect(json.length).to eq 62
+      end
+    end
+    context 'when the date is given' do
+      let(:params) { { date: '2021-09-06' } }
+      it 'returns menus of one day' do
+        expect(subject).to have_http_status(:ok)
+        # 2021年9月6日は月曜日で午前診療+午後診療で13枠
+        expect(json.length).to eq 13
+      end
     end
   end
 end
