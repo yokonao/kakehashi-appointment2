610b09e63d3dcaf65386e2d68f6e4ab77d6b8be9
yokonao <yokotukanao@me.com>
2021-08-02 22:50:14 +0900

    add some textfields

--- app/javascript/features/Form/form.tsx
+++ app/javascript/features/Form/form.tsx
@@ -1,5 +1,20 @@
-import * as React from 'react'
+import * as React from "react";
+import { Button, TextField } from "@material-ui/core";
 
-const Form = () => <div>フォーム</div>
+const Form = () => (
+  <div>
+    <div>
+      <TextField required id="last_name" label="姓" variant="outlined" />
+      <TextField required id="first_name" label="名" variant="outlined" />
+    </div>
+    <div>
+      <TextField id="last_kana_name" label="セイ" variant="outlined" />
+      <TextField id="first_kana_name" label="メイ" variant="outlined" />
+    </div>
+    <Button variant="contained" color="primary">
+      予約
+    </Button>
+  </div>
+);
 
-export default Form
+export default Form;
