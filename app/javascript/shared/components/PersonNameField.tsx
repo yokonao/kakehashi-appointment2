e43d0bf0972cf5389494702f65c702f30e4d6a82
yokonao <yokotukanao@me.com>
2021-08-04 22:23:55 +0900

    layout PersonNameFieldy

--- app/javascript/shared/components/PersonNameField.tsx
+++ app/javascript/shared/components/PersonNameField.tsx
@@ -1,4 +1,4 @@
-import { Box, TextField } from "@material-ui/core";
+import { Box, Grid, TextField } from "@material-ui/core";
 import * as React from "react";
 import { PersonName } from "../../domain/PersonName";
 
@@ -11,66 +11,74 @@ const PersonNameField = (props: Props) => {
   const { value, onChanged } = props;
   return (
     <Box m={2}>
-      <Box>
-        <TextField
-          required
-          id="last_name"
-          value={value.lastName}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            const newValue: PersonName = {
-              ...value,
-              lastName: e.target.value,
-            };
-            onChanged(newValue);
-          }}
-          label="姓"
-          variant="outlined"
-        />
-        <TextField
-          required
-          id="first_name"
-          value={value.firstName}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            const newValue: PersonName = {
-              ...value,
-              firstName: e.target.value,
-            };
-            onChanged(newValue);
-          }}
-          label="名"
-          variant="outlined"
-        />
-      </Box>
-      <Box>
-        <TextField
-          required
-          id="last_kana_name"
-          value={value.lastKanaName}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            const newValue: PersonName = {
-              ...value,
-              lastKanaName: e.target.value,
-            };
-            onChanged(newValue);
-          }}
-          label="セイ"
-          variant="outlined"
-        />
-        <TextField
-          required
-          id="first_kana_name"
-          value={value.firstKanaName}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            const newValue: PersonName = {
-              ...value,
-              firstKanaName: e.target.value,
-            };
-            onChanged(newValue);
-          }}
-          label="姓"
-          variant="outlined"
-        />
-      </Box>
+      <Grid container spacing={3}>
+        <Grid item>
+          <TextField
+            required
+            id="last_name"
+            value={value.lastName}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              const newValue: PersonName = {
+                ...value,
+                lastName: e.target.value,
+              };
+              onChanged(newValue);
+            }}
+            label="姓"
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            required
+            id="first_name"
+            value={value.firstName}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              const newValue: PersonName = {
+                ...value,
+                firstName: e.target.value,
+              };
+              onChanged(newValue);
+            }}
+            label="名"
+            variant="outlined"
+          />
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item>
+          <TextField
+            required
+            id="last_kana_name"
+            value={value.lastKanaName}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              const newValue: PersonName = {
+                ...value,
+                lastKanaName: e.target.value,
+              };
+              onChanged(newValue);
+            }}
+            label="セイ"
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            required
+            id="first_kana_name"
+            value={value.firstKanaName}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              const newValue: PersonName = {
+                ...value,
+                firstKanaName: e.target.value,
+              };
+              onChanged(newValue);
+            }}
+            label="姓"
+            variant="outlined"
+          />
+        </Grid>
+      </Grid>
     </Box>
   );
 };
