7a06b2a0340e154a47a1c3aa40f760a43f8f34ff
yokonao <yokotukanao@me.com>
2021-08-07 10:57:05 +0900

    integrate interface of BirthdayField and BirthdayFieldSimple

--- app/javascript/shared/components/BirthdayField.tsx
+++ app/javascript/shared/components/BirthdayField.tsx
@@ -2,14 +2,14 @@ import { Box, TextField } from "@material-ui/core";
 import { format, parse } from "date-fns";
 import * as React from "react";
 
-type BirthdayFieldProps = {
-  date: Date;
+type Props = {
+  value: Date;
   onChanged: (date?: Date) => void;
 };
 
-const BirthdayField = (props: BirthdayFieldProps) => {
-  const { date, onChanged } = props;
-  const [value, setValue] = React.useState("");
+const BirthdayField = (props: Props) => {
+  const { value, onChanged } = props;
+  const [rawValue, setRawValue] = React.useState("");
   const [errorMessages, setErrorMessages] = React.useState<string[]>([]);
 
   return (
@@ -17,20 +17,20 @@ const BirthdayField = (props: BirthdayFieldProps) => {
       <TextField
         required
         id="birthday"
-        value={date ? format(date, "yyyy年MM月dd日") : value}
+        value={value ? format(value, "yyyy年MM月dd日") : rawValue}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
           const input = e.target.value;
           const res = /^[0-9]{4}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])$/.test(
             input
           );
           if (!res) {
-            setValue(input);
+            setRawValue(input);
             setErrorMessages([...errorMessages, "8桁の数字を入力してください"]);
             return;
           }
           const date = parse(input, "yyyyMMdd", new Date())
           if (isNaN(date.getTime())) {
-            setValue(input);
+            setRawValue(input);
             setErrorMessages([...errorMessages, "不正な日付です"]);
             return;
           }
@@ -40,7 +40,7 @@ const BirthdayField = (props: BirthdayFieldProps) => {
         label="生年月日"
         variant="outlined"
         error={errorMessages.length > 0}
-        disabled={!!date}
+        disabled={!!value}
       />
     </Box>
   );
