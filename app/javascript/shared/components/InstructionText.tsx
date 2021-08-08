635e8a7dc0f79c10c62e8c580081d8cc62206de3
yokonao <yokotukanao@me.com>
2021-08-09 05:49:37 +0900

    instruction text font weight

--- app/javascript/shared/components/InstructionText.tsx
+++ app/javascript/shared/components/InstructionText.tsx
@@ -9,7 +9,9 @@ const InstructionText = (props: Props) => {
   const { text } = props;
   return (
     <Box py={2} my={2}>
-      <Typography color="inherit">{text}</Typography>
+      <Typography color="inherit">
+        <Box fontWeight={500}>{text}</Box>
+      </Typography>
     </Box>
   );
 };
