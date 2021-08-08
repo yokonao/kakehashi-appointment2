5562750911aa05168cb0ff8022b32a60e2d1327f
yokonao <yokotukanao@me.com>
2021-08-09 07:29:00 +0900

    add validation against email

--- app/javascript/shared/components/EmailInput.tsx
+++ app/javascript/shared/components/EmailInput.tsx
@@ -1,5 +1,8 @@
 import { Box, TextField } from "@material-ui/core";
 import * as React from "react";
+import useFormElementState from "../../features/hooks/useFormElementState";
+import CheckMark from "./CheckMark";
+import ErrorMessages from "./ErrorMessages";
 
 type Props = {
   value: string;
@@ -7,6 +10,23 @@ type Props = {
 };
 const EmailInput = (props: Props) => {
   const { value, onChanged } = props;
+  const { state, verify, addErrorMessage } = useFormElementState();
+  const validate = React.useCallback(() => {
+    if (!value || value.length == 0) {
+      addErrorMessage("メールアドレスを入力してください");
+      return;
+    }
+    // TODO メールアドレスのバリデーションはちゃんと考えてテストしなければならない
+    if (
+      !/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+        value
+      )
+    ) {
+      addErrorMessage("不正なメールアドレスです");
+      return;
+    }
+    verify();
+  }, [value, verify, addErrorMessage]);
   return (
     <Box m={2}>
       <TextField
@@ -14,11 +34,21 @@ const EmailInput = (props: Props) => {
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
           onChanged(e.target.value);
         }}
+        onBlur={() => {
+          validate();
+        }}
         type="email"
         placeholder="test@example.com"
         helperText="メールアドレス"
         variant="outlined"
+        InputProps={{
+          endAdornment: state.isValid && <CheckMark />,
+        }}
+        error={
+          state.errorMessages.length > 0 || state.externalErrors.length > 0
+        }
       />
+      <ErrorMessages messages={state.errorMessages} />
     </Box>
   );
 };
