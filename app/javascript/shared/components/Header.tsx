0a2e26f8dc28d408aae5f7891b9f68880532ff15
yokonao <yokotukanao@me.com>
2021-08-09 15:59:06 +0900

    add link text

--- app/javascript/shared/components/Header.tsx
+++ app/javascript/shared/components/Header.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Box, Toolbar, Typography } from "@material-ui/core";
 import { Logo, LogoName } from "./Logo";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <AppBar position="static" color="default">
       <Toolbar>
         <Logo />
-        <LogoName/>
+        <LogoName />
+        <Box m={2}>
+          <Link to="/form/internal_medicine">内科</Link>
+        </Box>
+        <Box m={2}>
+          <Link to="/form/kampo">漢方</Link>
+        </Box>
       </Toolbar>
     </AppBar>
   );
