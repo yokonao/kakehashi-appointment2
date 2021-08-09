5cea1046b95f4e8f727cedb25e5064a266f5e74c
yokonao <yokotukanao@me.com>
2021-08-09 23:16:55 +0900

    implement appointment mail generator

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -1,2 +1,7 @@
 class AppointmentMailer < ApplicationMailer
+  def appointment_email
+    @appointment = params[:appointment]
+    @menu = @appointment.menu
+    mail(to: @appointment.email, subject: '予約が成立しました')
+  end
 end
