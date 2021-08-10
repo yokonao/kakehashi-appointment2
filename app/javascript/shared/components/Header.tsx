35409846f5ca48164ba9324860ebb5058bf58102
yokonao <yokotukanao@me.com>
2021-08-10 21:20:11 +0900

    improve: yresponsive Header

--- app/javascript/shared/components/Header.tsx
+++ app/javascript/shared/components/Header.tsx
@@ -1,20 +1,25 @@
 import * as React from "react";
-import { AppBar, Box, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Box, Theme, Toolbar, Typography, useMediaQuery } from "@material-ui/core";
 import { Logo, LogoName } from "./Logo";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const isMobile = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.down('xs')
+  );
   return (
     <AppBar position="static" color="default">
       <Toolbar>
-        <Logo />
-        <LogoName />
-        <Box m={2}>
+        <Logo isMobile={isMobile}/>
+        <Typography color="primary" variant="h6">
+          かけはし糖尿病・甲状腺クリニック
+        </Typography>
+        {/* <Box m={2}>
           <Link to="/form/internal_medicine">内科</Link>
         </Box>
         <Box m={2}>
           <Link to="/form/kampo">漢方</Link>
-        </Box>
+        </Box> */}
       </Toolbar>
     </AppBar>
   );
