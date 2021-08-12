e73090b4cfb0bbdabae1daca5eaa51249057ef6e
yokonao <yokotukanao@me.com>
2021-08-13 08:42:51 +0900

    add spec for redirecting

--- spec/requests/admin_spec.rb
+++ spec/requests/admin_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Admins", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /admin/dashboard" do
+    it 'redirects to login page' do
+      get '/admin/dashboard'
+      expect(response.status).to eq 302
+      expect(response).to redirect_to new_administrator_session_url
+    end
   end
 end
