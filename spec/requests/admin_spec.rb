18b9450cbb91cca30bec40f8bb41e963815fe68e
yokonao <yokotukanao@me.com>
2021-08-13 08:47:24 +0900

    add spec when logged in

--- spec/requests/admin_spec.rb
+++ spec/requests/admin_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "Admins", type: :request do
-  describe "GET /admin/dashboard" do
-    it 'redirects to login page' do
-      get '/admin/dashboard'
-      expect(response.status).to eq 302
-      expect(response).to redirect_to new_administrator_session_url
+RSpec.describe 'Admins', type: :request do
+  describe 'GET /admin/dashboard' do
+    context 'when not login' do
+      it 'redirects to login page' do
+        get '/admin/dashboard'
+        expect(response.status).to eq 302
+        expect(response).to redirect_to new_administrator_session_url
+      end
+    end
+
+    context 'when login' do
+      let(:administrator) { create(:administrator) }
+      before do
+        sign_in administrator
+      end
+      it 'return 200' do
+        get '/admin/dashboard'
+        expect(response).to have_http_status :ok
+      end
     end
   end
 end
