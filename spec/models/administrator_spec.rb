8c25432ab8219b0c46d7efdfcf4e15091efc699b
yokonao <yokotukanao@me.com>
2021-08-13 08:36:42 +0900

    add spec for administrator

--- spec/models/administrator_spec.rb
+++ spec/models/administrator_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Administrator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid when email does not exist' do
+    admin = build(:administrator, email: nil)
+    expect(admin.valid?).to eq false
+  end
+
+  it 'is invalid when password does not exist' do
+    admin = build(:administrator, password: nil)
+    expect(admin.valid?).to eq false
+  end
+  
+  it 'is invalid when email is blank' do
+    admin = build(:administrator, email: '')
+    expect(admin.valid?).to eq false
+  end
+
+  it 'is invalid when password is blank' do
+    admin = build(:administrator, password: '')
+    expect(admin.valid?).to eq false
+  end
 end
