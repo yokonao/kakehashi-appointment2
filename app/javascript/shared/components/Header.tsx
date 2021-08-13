b7d2675f3f20717f64fac94e821d2c36ce87f441
yokonao <yokotukanao@me.com>
2021-08-14 01:18:36 +0900

    in mobile, small title header

--- app/javascript/shared/components/Header.tsx
+++ app/javascript/shared/components/Header.tsx
@@ -1,16 +1,22 @@
 import * as React from "react";
-import { AppBar, Theme, Toolbar, Typography, useMediaQuery } from "@material-ui/core";
+import {
+  AppBar,
+  Theme,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+} from "@material-ui/core";
 import { Logo } from "./Logo";
 
 const Header = () => {
   const isMobile = useMediaQuery((theme: Theme) =>
-    theme.breakpoints.down('xs')
+    theme.breakpoints.down("xs")
   );
   return (
     <AppBar position="static" color="default">
       <Toolbar>
-        <Logo isMobile={isMobile}/>
-        <Typography color="primary" variant="h6">
+        <Logo isMobile={isMobile} />
+        <Typography color="primary" variant={isMobile ? "subtitle2" : "h6"}>
           かけはし糖尿病・甲状腺クリニック
         </Typography>
       </Toolbar>
