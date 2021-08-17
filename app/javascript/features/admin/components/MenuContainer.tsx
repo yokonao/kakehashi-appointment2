56db3309ea5cd6493862eb93f3c7b32a729edfcc
yokonao <yokotukanao@me.com>
2021-08-18 00:34:28 +0900

    delete dead code

--- app/javascript/features/admin/components/MenuContainer.tsx
+++ app/javascript/features/admin/components/MenuContainer.tsx
@@ -1,27 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import { Typography } from "@material-ui/core";
-import { format, startOfWeek, addDays } from "date-fns";
-import { ja } from "date-fns/locale";
 import WeeklyMenu from "./WeeklyMenu";
-import { MenuAdminSerializer } from "../../../serializers/MenuAdminSerializer";
 import { useAdminContext } from "../hooks/useAdminContext";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    border: "thick double #99720f",
-    height: 500,
-    width: 120,
-  },
-  control: {
-    padding: theme.spacing(1),
-  },
-}));
 
 const MenuContainer = () => {
   const { menus } = useAdminContext();
