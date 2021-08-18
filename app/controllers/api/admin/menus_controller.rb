cb32ad700c4fbd20a385eb3689e62969207d07f8
yokonao <yokotukanao@me.com>
2021-08-19 00:05:14 +0900

    add min_date and max_date to destroy_all menus

--- app/controllers/api/admin/menus_controller.rb
+++ app/controllers/api/admin/menus_controller.rb
@@ -18,7 +18,18 @@ class Api::Admin::MenusController < ApplicationController
   end
 
   def destroy_all
-    Menu.all.destroy_all
-    render json: { "message": '予約枠を削除しました' }
+    menus = Menu.all.preload(:appointment)
+    min_date = date_params[:min_date]
+    max_date = date_params[:max_date]
+    menus = menus.where(start_at: Time.parse(min_date)..) if min_date
+    menus = menus.where(start_at: ..Time.parse(max_date).end_of_day) if max_date
+    before_count = Menu.count
+    menus.destroy_all
+    after_count = Menu.count
+    render json: { "message": "#{after_count - before_count}件の予約枠を削除しました" }
+  end
+
+  def date_params
+    params.permit(:min_date, :max_date)
   end
 end
