ef852a7ae8e3cf61cd11550b4648d204483f52d5
yokonao <yokotukanao@me.com>
2021-08-18 23:21:37 +0900

    implement ConfirmationDialog

--- app/javascript/features/admin/api/AdminApiClient.ts
+++ app/javascript/features/admin/api/AdminApiClient.ts
@@ -2,7 +2,10 @@ import {
   AppointmentSerializer,
   castToAppointmentSerializer,
 } from "../../../serializers/AppointmentSerializer";
-import { castToMenuAdminSerializer, MenuAdminSerializer } from "../../../serializers/MenuAdminSerializer";
+import {
+  castToMenuAdminSerializer,
+  MenuAdminSerializer,
+} from "../../../serializers/MenuAdminSerializer";
 import client from "../../../shared/api/client";
 
 export class AdminApiClient {
@@ -41,6 +44,20 @@ export class AdminApiClient {
       return { success: true, data: appointments };
     } catch (err) {
       return { success: false, data: [] };
+    }
+  }
+
+  static async deleteMenu(id: number): Promise<{ message: string }> {
+    try {
+      const res = await client.delete(
+        `/api/admin/appointments/${id.toString()}`
+      );
+      if (res.status !== 200) {
+        return { message: "予約枠の削除に失敗しました" };
+      }
+      return { message: "予約枠を削除しました" };
+    } catch (err) {
+      return { message: "予約枠の削除に失敗しました" };
     }
   }
 }
---
+++ app/javascript/shared/components/ConfirmationDialog.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+} from "@material-ui/core";
+type Props = {
+  title: string;
+  open: boolean;
+  onOk: () => void;
+  onCancel: () => void;
+  okButtonColor: "primary" | "secondary";
+  okButtonText: string;
+  cancelButtonText: string;
+  children?: React.ReactNode;
+};
+
+const ConfirmationDialog = (props: Props) => {
+  return (
+    <Dialog open={props.open} onClose={props.onCancel}>
+      <DialogTitle>{props.title}</DialogTitle>
+      <DialogContent>{props.children}</DialogContent>
+      <DialogActions>
+        <Button onClick={props.onCancel} color="default">
+          {props.cancelButtonText}
+        </Button>
+        <Button onClick={props.onOk} color={props.okButtonColor}>
+          {props.okButtonText}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
