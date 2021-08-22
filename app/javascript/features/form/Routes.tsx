6e490a3c8b39b4d91bb423aba1365a3e3a0103f9
yokonao <yokotukanao@me.com>
2021-08-22 22:27:46 +0900

    add DepartmentChoice

--- app/javascript/features/form/Routes.tsx
+++ app/javascript/features/form/Routes.tsx
@@ -3,21 +3,25 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import { useMenusContext } from "./hooks/useMenusContext";
 import InternalMedicineFormContainer from "./components/InternalMedicineFormContainer";
 import KampoFormContainer from "./components/KampoFormContainer";
+import DepartmentChoice from "./components/DepartmentChoice";
 
 const Routes = () => {
   const { internalMedicineMenus, kampoMenus, isLoading } = useMenusContext();
   return (
     <Switch>
-      <Route path="/form/internal_medicine">
+      <Route path="/" exact>
+        <DepartmentChoice />
+      </Route>
+      <Route path="/form/internal_medicine" exact>
         <InternalMedicineFormContainer
           menus={internalMedicineMenus}
           isLoading={isLoading}
         />
       </Route>
-      <Route path="/form/kampo">
+      <Route path="/form/kampo" exact>
         <KampoFormContainer menus={kampoMenus} isLoading={isLoading} />
       </Route>
-      <Redirect to="/form/internal_medicine" />
+      <Redirect to="/" />
     </Switch>
   );
 };
---
+++ app/javascript/features/form/components/DepartmentChoice.tsx
@@ -0,0 +1,50 @@
+import { Box, Button, Container, Grid, Typography } from "@material-ui/core";
+import * as React from "react";
+import { useHistory } from "react-router-dom";
+
+const DepartmentChoice = () => {
+  const history = useHistory();
+  return (
+    <>
+      <Container maxWidth="xs" style={{ backgroundColor: "white" }}>
+        <Box py={2} my={2} textAlign="center">
+          <Typography variant="h4" color="primary">
+            診療科を選択
+          </Typography>
+        </Box>
+        <Typography>2週間後までの予約をお取りいただけます</Typography>
+        <Grid
+          container
+          spacing={1}
+          style={{ paddingTop: "2rem", paddingBottom: "2rem" }}
+        >
+          <Grid item xs={6}>
+            <Button
+              color="primary"
+              variant="outlined"
+              fullWidth
+              size="large"
+              onClick={() => history.push("/form/internal_medicine")}
+            >
+              内科
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button
+              color="primary"
+              variant="outlined"
+              fullWidth
+              size="large"
+              onClick={() => history.push("/form/kampo")}
+            >
+              漢方
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+      <div style={{ height: "400px" }} />
+    </>
+  );
+};
+
+export default DepartmentChoice;
