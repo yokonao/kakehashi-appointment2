b36af92d299b8717f7daad2e1cbfaf232864fa9a
yokonao <yokotukanao@me.com>
2021-08-24 00:03:17 +0900

    click '内科'ボタン

--- spec/system/make_appointment_spec.rb
+++ spec/system/make_appointment_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Appointment, type: :system do
   context 'when input parameters is valid' do
-    it 'makes an appointment for internal medicine' do
+    it 'makes an appointment for internal medicine', js: true do
       visit '/'
-      
+      expect(page).to have_text('診療科を選択')
+      click_button '内科'
+      expect(page).to have_text('内科外来予約')
+      sleep 5
     end
   end
 end
