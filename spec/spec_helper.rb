8f4a2499fe539db9b6167ffa2ddc163be3e50347
yokonao <yokotukanao@me.com>
2021-08-23 20:04:53 +0900

    add visit only spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,7 +50,7 @@ RSpec.configure do |config|
   # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
   config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless　　
+    driven_by :selenium_chrome_headless
   end
   #   # This allows you to limit a spec run to individual examples or groups
   #   # you care about by tagging them with `:focus` metadata. When nothing
---
+++ spec/system/make_appointment_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Appointment, type: :system do
+  context 'when input parameters is valid' do
+    it 'makes an appointment for internal medicine' do
+      visit '/'
+      
+    end
+  end
+end
